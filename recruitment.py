1312e2e736075ed4d3a38eeb05c678ae9261f112
Mohammad Al-Thunayan <malthunayan4@gmail.com>
2022-03-20 01:06:32 +0300

    Update task skeleton

--- recruitment.py
+++ recruitment.py
@@ -1,5 +1,32 @@
-def main():
-	#write your code here
+def show_skills(skills):
+    # Pretty print skills to the user
+    # Write your code here
+    ...
 
-if __name__ == '__main__':
-	main()
+
+def get_user_skills(skills):
+    # Show the available skills and have user pick from them
+    # Write your code here
+    ...
+
+
+def get_user_cv(skills):
+    # Get the users CV from their inputs
+    # Write your code here
+    ...
+
+
+def check_acceptance(cv, desired_skill):
+    # Check if the cv is acceptable or not and return a boolean based on that
+    # Write your code here
+    ...
+
+
+def main():
+    # Write your main logic here by combining the functions above into the
+    # desired outcome
+    ...
+
+
+if __name__ == "__main__":
+    main()
